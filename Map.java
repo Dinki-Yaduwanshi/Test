4459143ac044c8c7ca2934d10bf805ea73621473
Dinki-Yaduwanshi <zmeet250@gmail.com>
2023-08-28 12:36:00 +0530

    Question asked

--- Map.java
+++ Map.java
@@ -1,6 +1,36 @@
-import java.util.*;
 
+
+import java.util.*;
 class Map {
+
+ public static void checkUnique(String a){
+Map<Character, Integer> map = new HashMap<>();
+int i=0;
+int size= a.length();
+while(i!=size){
+char c = a.charAt(i);
+int count= map.getOrDefault(c,0);
+map.put(c,count+1);
+
+i++;
+}
+
+Set<Map.Entry<Character, Integer>> hmap= map.entrySet();
+
+for(Map.Entry<Character, Integer> data: hmap){
+ if(data.getValue()>1){
+System.out.print("not unique");
+System.exit(0);
+}
+
+}
+
+System.out.print("unique");
+}
+
+
+
+
     public static void mapIt(String a) {
         HashMap<Character, Integer> hm = new HashMap<>();
         int i = 0;
@@ -19,7 +49,8 @@ class Map {
     }
 
     public static void main(String args[]) {
-        String a = "XYZ";
-        mapIt(a);
+        String a = "spiderman";
+       // mapIt(a);
+        checkUnique(a);
     }
 }
